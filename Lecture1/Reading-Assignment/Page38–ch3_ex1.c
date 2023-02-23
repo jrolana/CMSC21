6bfe3a0f8001e8d8c947e6b52a08ba256dc24844
jrolana <jrolana@up.edu.ph>
2023-02-23 17:41:57 +0800

    Initial commit

---
+++ Lecture1/Reading-Assignment/Page38–ch3_ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Prints integers and floats.
+int main(void) {
+    // Declaration of variables with data types integer and float.
+    int i, j;
+    float x, y;
+
+    // Gives values to the declared variables.
+    i = 10;
+    j = 20;
+    x = 43.2892f;
+    y = 5527.0f;
+
+    // Prints the characters in the strings and the values of the declared variables.
+    printf("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);
+}
